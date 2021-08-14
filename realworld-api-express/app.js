f75ae78c14d2aafde46be195e7ca402a414a463c
Sunweijun333 <2318085206.qq.com>
2021-08-14 15:16:40 +0800

    配置常见中间件

--- realworld-api-express/app.js
+++ realworld-api-express/app.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+// 配置中间件
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
     res.send('Hello world')
 })
 
